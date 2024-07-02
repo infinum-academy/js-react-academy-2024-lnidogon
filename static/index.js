617fdb5a9ed587f926dad41037a2a1af3e3824d1
Lovro <lovro.nidogon@gmail.com>
2024-07-02 16:33:54 +0200

    Dodana osnovna JS funkcionalnost (dodavanje i loadanje stranice)

---
+++ static/index.js
@@ -0,0 +1,32 @@
+let reviewArray = [{text: "proba", rank: 4}];
+
+function createReviewElement(review) {
+    const reviewEl = document.createElement(`div`);
+    reviewEl.classList = ['review'];
+    const reviewText = document.createElement('span');
+    reviewText.innerHTML = review.text;
+    const reviewRank = document.createElement(`span`);
+    reviewRank.innerHTML = review.rank + '/5';
+    reviewEl.appendChild(reviewText);
+    reviewEl.appendChild(reviewRank);
+    return reviewEl;
+}
+
+
+function renderPage() {
+    const reviewList = document.getElementById(`reviews`);
+    reviewList.innerHTML = "";
+    reviewArray.forEach((review) => {
+        reviewList.appendChild(createReviewElement(review));
+    });
+}
+
+function addReview() {
+    const reviewTextInput = document.getElementById('review-text-input');
+    const reviewRankInput = document.getElementById(`review-rank-input`);
+    reviewArray.push({text: reviewTextInput.value, rank: reviewRankInput.value});
+    renderPage();
+}
+
+renderPage();
+
