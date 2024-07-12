b61407023c1132859c242189afb09503bd1aa802
Lovro Nidogon <lovro.nidogon@gmail.com>
2024-07-12 16:31:43 +0200

    Napravljen neki progress

---
+++ tv_shows_app/src/app/register/page.tsx
@@ -0,0 +1,5 @@
+import { RegisterForm } from '@/components/features/auth/RegisterForm/RegisterForm';
+
+export default function RegisterPage() {
+  <RegisterForm />;
+}
---
+++ tv_shows_app/src/components/features/auth/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,5 @@
+import { Flex } from '@chakra-ui/react';
+
+export const RegisterForm = () => {
+  return <Flex direction="column" alignItems="center"></Flex>;
+};
