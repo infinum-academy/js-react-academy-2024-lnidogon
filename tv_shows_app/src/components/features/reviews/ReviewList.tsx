18bc93604691b62b38fedabc9ed1532a89395f3a
Vedran <vedran.serences@infinum.hr>
2024-07-15 11:15:21 +0200

    Fix merge conflict

--- tv_shows_app/src/components/features/reviews/ReviewList.tsx
+++ tv_shows_app/src/components/features/reviews/ReviewList.tsx
@@ -4,59 +4,52 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 
 interface IReviewsList {
-  reviews: Array<IReview>;
-  onRemove: (review: IReview) => void;
+	reviews: Array<IReview>;
+	onRemove: (review: IReview) => void;
 }
 
 export const ReviewList = ({ reviews, onRemove }: IReviewsList) => {
-  const [openPage, setOpenPage] = useState(0);
-  const maxPage = Math.ceil(reviews.length / 3);
+	const [openPage, setOpenPage] = useState(0);
+	const maxPage = Math.ceil(reviews.length / 3);
 
-  const changePage = (increment: number) => {
-    if (openPage + increment >= maxPage) setOpenPage(0);
-    else if (openPage + increment < 0) setOpenPage(maxPage - 1);
-    else setOpenPage(openPage + increment);
-  };
-  return (
-    <Flex flexDirection="column" gap="2" width="100%" marginBottom="3">
-      {reviews
-        .filter(
-          (el, index) => index >= openPage * 3 && index < openPage * 3 + 3
-        )
-        .map((review, index) => {
-          return <ReviewItem key={index} review={review} onRemove={onRemove} />;
-        })}
-      <Flex
-        flexDirection="row"
-        alignSelf="center"
-        alignItems="center"
-        marginTop="auto"
-      >
-        <IconButton
-          marginLeft="auto"
-          aria-label="Previous comment"
-          backgroundColor="pink.900"
-          color="white"
-          icon={<ChevronLeftIcon />}
-          size="sm"
-          _hover={{ backgroundColor: 'pink.900' }}
-          onClick={() => changePage(-1)}
-        />
-        <Text color="white" fontSize="xs">
-          {maxPage ? openPage + 1 + ' od ' + maxPage : 'no reviews'}
-        </Text>
+	const changePage = (increment: number) => {
+		if (openPage + increment >= maxPage) setOpenPage(0);
+		else if (openPage + increment < 0) setOpenPage(maxPage - 1);
+		else setOpenPage(openPage + increment);
+	};
+	return (
+		<Flex flexDirection="column" gap="2" width="100%" marginBottom="3">
+			{reviews
+				.filter((el, index) => index >= openPage * 3 && index < openPage * 3 + 3)
+				.map((review, index) => {
+					return <ReviewItem key={index} review={review} onRemove={onRemove} />;
+				})}
+			<Flex flexDirection="row" alignSelf="center" alignItems="center" marginTop="auto">
+				<IconButton
+					marginLeft="auto"
+					aria-label="Previous comment"
+					backgroundColor="pink.900"
+					color="white"
+					icon={<ChevronLeftIcon />}
+					size="sm"
+					_hover={{ backgroundColor: 'pink.900' }}
+					onClick={() => changePage(-1)}
+				/>
+				<Text color="white" fontSize="xs">
+					{maxPage ? openPage + 1 + ' od ' + maxPage : 'no reviews'}
+				</Text>
 
-        <IconButton
-          marginLeft="auto"
-          aria-label="Next comment"
-          backgroundColor="pink.900"
-          color="white"
-          icon={<ChevronRightIcon />}
-          size="sm"
-          _hover={{ backgroundColor: 'pink.900' }}
-          onClick={() => changePage(1)}
-        />
-      </Flex>
-    </Flex>
-  );
+				<IconButton
+					marginLeft="auto"
+					aria-label="Next comment"
+					backgroundColor="pink.900"
+					color="white"
+					icon={<ChevronRightIcon />}
+					size="sm"
+					_hover={{ backgroundColor: 'pink.900' }}
+					onClick={() => changePage(1)}
+				/>
+			</Flex>
+		</Flex>
+	);
 };
--- tv_shows_app/src/components/features/shows/ReviewForm.tsx
+++ tv_shows_app/src/components/features/shows/ReviewForm.tsx
@@ -1,75 +1,67 @@
-import {
-  Input,
-  Button,
-  Flex,
-  Textarea,
-  Image,
-  Container,
-  Text,
-} from '@chakra-ui/react';
+import { Input, Button, Flex, Textarea, Image, Container, Text } from '@chakra-ui/react';
 import { IReview } from '../reviews/ReviewItem';
 import { useState } from 'react';
-import { setEngine } from 'crypto';
-import { RatingInput } from '../reviews/RatingInput';
+import { StarReview } from '../reviews/StarReview';
 
 export interface IReviewFormProps {
-  onAdd: (review: IReview) => void;
+	onAdd: (review: IReview) => void;
 }
 
 export const ReviewForm = ({ onAdd }: IReviewFormProps) => {
-  const [selectedNumberOfStars, setSelectedNumberOfStars] = useState(0);
+	let starArray = [];
+	const [locked, setLocked] = useState(false);
+	const [selectedNumberOfStars, setSelectedNumberOfStars] = useState(0);
+	const [hoveredNumberOfStars, setHoveredNumberOfStars] = useState(0);
 
-  const onClickHandler = () => {
-    const reviewInput = document.getElementById(
-      'review-input'
-    ) as HTMLInputElement;
-    if (reviewInput.value == '' || selectedNumberOfStars == 0) return;
-    onAdd({
-      email: '',
-      avatarUrl: '',
-      comment: reviewInput.value,
-      rating: selectedNumberOfStars,
-    });
-    reviewInput.value = '';
-  };
-  return (
-    <Flex flexDirection="column" gap="2" width="100%" overflow="hidden">
-      <Textarea
-        backgroundColor="orange.100"
-        placeholder="Add review..."
-        id="review-input"
-        width="100%"
-        fontSize="sm"
-        padding="2"
-        height="40px"
-        minHeight="40px"
-        borderRadius="7"
-        fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
-      />
-      <Flex
-        flexDirection="row"
-        alignItems="center"
-        width="100%"
-        gap="5"
-        height="8"
-      >
-        <Text color="white" fontSize="sm">
-          Rating:
-        </Text>
-        <RatingInput setNumberOfStarsSelected={setSelectedNumberOfStars} />
-        <Button
-          fontSize="sm"
-          onClick={onClickHandler}
-          height="7"
-          width="100px"
-          borderRadius="15px"
-          _hover={{ backgroundColor: 'green.300' }}
-          backgroundColor="orange.100"
-          marginLeft="auto"
-        >
-          Post
-        </Button>
-      </Flex>
-    </Flex>
-  );
+	const onClickHandler = () => {
+		const reviewInput = document.getElementById('review-input') as HTMLInputElement;
+		if (reviewInput.value == '' || selectedNumberOfStars == 0) return;
+		onAdd({
+			email: '',
+			avatarUrl: '',
+			comment: reviewInput.value,
+			rating: selectedNumberOfStars,
+		});
+		setSelectedNumberOfStars(0);
+	};
+	const onClick = (index: number) => {
+		setSelectedNumberOfStars(index);
+	};
+	const onHover = (index: number) => {
+		setHoveredNumberOfStars(index);
+	};
+	return (
+		<Flex flexDirection="column" gap="5" width="100%">
+			<Textarea
+				backgroundColor="orange.100"
+				placeholder="Add review..."
+				id="review-input"
+				width="100%"
+				fontSize="14"
+				padding="5"
+				height="70px"
+				minHeight="70px"
+				resize="vertical"
+				borderRadius="7"
+				fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
+			/>
+			<Flex width="100%" onMouseLeave={() => setLocked(true)} onMouseEnter={() => setLocked(false)}>
+				<StarReview
+					noOfStars={locked ? selectedNumberOfStars : hoveredNumberOfStars}
+					isStatic={false}
+					onChange={onClick}
+					onHover={onHover}
+				/>
+			</Flex>
+			<Button
+				onClick={onClickHandler}
+				width="16%"
+				borderRadius="15px"
+				_hover={{ backgroundColor: 'green.300' }}
+				backgroundColor="orange.100"
+			>
+				Post
+			</Button>
+		</Flex>
+	);
 };
