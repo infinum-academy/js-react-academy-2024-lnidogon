731004661ef075d2eff0913a32a5ec790d8868a5
Lovro Nidogon <lovro.nidogon@gmail.com>
2024-07-16 12:50:09 +0200

    Malo preuređeno da izgleda kao original nakon merganja

--- tv_shows_app/src/components/features/reviews/StarReview.tsx
+++ tv_shows_app/src/components/features/reviews/StarReview.tsx
@@ -1,41 +1,43 @@
 import { StarIcon } from '@chakra-ui/icons';
 import { Container, Flex, Icon, Image } from '@chakra-ui/react';
+import { cursorTo } from 'readline';
 
 interface IStarReview {
-	noOfStars: number;
-	isStatic: boolean;
-	onChange: (index: number) => void;
-	onHover: (index: number) => void;
+  noOfStars: number;
+  isStatic: boolean;
+  onChange: (index: number) => void;
+  onHover: (index: number) => void;
 }
 
 export const StarReview = (starReview: IStarReview) => {
-	let tempList = [];
-	for (let i = 1; i <= 5; i++) {
-		tempList.push(
-			<StarIcon
-				color={i <= starReview.noOfStars ? 'yellow' : 'gray'}
-				width="15%"
-				key={i}
-				onClick={
-					starReview.isStatic
-						? () => {}
-						: () => {
-								starReview.onChange(i);
-							}
-				}
-				onMouseOver={
-					starReview.isStatic
-						? () => {}
-						: () => {
-								starReview.onHover(i);
-							}
-				}
-			/>
-		);
-	}
-	return (
-		<Flex flexDirection="row" gap="0" width="30%" marginTop="1" marginBottom="3">
-			{tempList}
-		</Flex>
-	);
+  let tempList = [];
+  for (let i = 1; i <= 5; i++) {
+    tempList.push(
+      <StarIcon
+        _hover={{ cursor: starReview.isStatic ? '' : 'pointer' }}
+        color={i <= starReview.noOfStars ? 'yellow' : 'gray'}
+        width="15%"
+        key={i}
+        onClick={
+          starReview.isStatic
+            ? () => {}
+            : () => {
+                starReview.onChange(i);
+              }
+        }
+        onMouseOver={
+          starReview.isStatic
+            ? () => {}
+            : () => {
+                starReview.onHover(i);
+              }
+        }
+      />
+    );
+  }
+  return (
+    <Flex flexDirection="row" gap="0" width="30%" alignItems="center">
+      {tempList}
+    </Flex>
+  );
 };
